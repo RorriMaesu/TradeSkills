ec8cf0013dad72b3f7b75ff4327895db484f389c
Andrew Green <andrew.green.contact@gmail.com>
2025-04-14 17:31:02 -0700

    Add local development server scripts to fix CORS issues

---
+++ start-node-server.bat
@@ -0,0 +1,11 @@
+@echo off
+echo Installing dependencies (first time only)...
+npm install
+
+echo.
+echo Starting local development server for TradeSkills...
+echo.
+echo Your site will be available at: http://localhost:8000
+echo Press Ctrl+C to stop the server
+echo.
+npm start
---
+++ start-server.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Starting local development server for TradeSkills...
+echo.
+echo Your site will be available at: http://localhost:8000
+echo Press Ctrl+C to stop the server
+echo.
+python -m http.server 8000
